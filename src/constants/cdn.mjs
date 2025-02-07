807f2ad8786f70bc32deec910a0360854736e36f
HPCesia <me@hpcesia.com>
2025-02-07 17:59:10 +0800

    refactor: migrate CDN to mjs

---
+++ src/constants/cdn.mjs
@@ -0,0 +1,4 @@
+export const CDN = {
+  twikoo: 'https://cdn.jsdelivr.net/npm/twikoo@1.6.41/dist/twikoo.nocss.js',
+  giscus: 'https://giscus.app/client.js',
+};
--- src/constants/cdn.ts
+++
@@ -1,4 +0,0 @@
-export const CDN = {
-  twikoo: 'https://registry.npmmirror.com/twikoo/1.6.41/files/dist/twikoo.nocss.js',
-  giscus: 'https://giscus.app/client.js',
-};
