4dba66d6d36685b32f8467e0407ba7c84838b38d
HPCesia <me@hpcesia.com>
2025-02-09 22:39:21 +0800

    refactor: remove unused type

--- src/types/Errors.ts
+++
@@ -1,9 +0,0 @@
-import { AstroError } from 'astro/errors';
-
-export class AstroParameterConflictError extends AstroError {
-  constructor(...params: string[]) {
-    const message = `The following parameters are in conflict: ${params.join(', ')}. Please ensure that only one of these parameters is provided.`;
-    super(message);
-    this.name = 'ParameterConflictError';
-  }
-}
