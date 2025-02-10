853a8ce292dc0dd5326651ad2a2a305fb7f913b9
HPCesia <me@hpcesia.com>
2025-02-10 16:11:44 +0800

    feat: rss content

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,6 +2,10 @@ import { siteConfig } from '@/config';
 import rss from '@astrojs/rss';
 import { getSortedPosts } from '@utils/content-utils';
 import type { AstroGlobal } from 'astro';
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
+
+const parser = new MarkdownIt();
 
 export async function GET(context: AstroGlobal) {
   const posts = await getSortedPosts();
@@ -9,11 +13,15 @@ export async function GET(context: AstroGlobal) {
     title: siteConfig.title,
     description: siteConfig.subtitle,
     site: context.site || '',
+    stylesheet: '/rss/styles.xsl',
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.published,
       description: post.data.description,
       link: `/posts/${post.data.slug}`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
     })),
   });
 }
