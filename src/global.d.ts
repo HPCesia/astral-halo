7f7d57b94fdab9aafce52ce531e246cf420ba2e0
HPCesia <me@hpcesia.com>
2025-03-18 19:56:48 +0800

    refactor: declare twikoo global variable

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,4 +4,39 @@ declare global {
   interface Window {
     swup: Swup;
   }
+
+  const twikoo: {
+    init: (options: {
+      envId: string;
+      el: string;
+      region?: string;
+      path?: string;
+      lang?: string;
+      onCommentLoaded?: () => void;
+    }) => Promise<void>;
+    getCommentsCount: (options: {
+      envId: string;
+      urls: string[];
+      includeReply?: boolean;
+    }) => Promise<{ url: string; count: number }[]>;
+    getRecentComments: (options: {
+      envId: string;
+      urls?: string[];
+      pageSize?: number;
+      includeReply?: boolean;
+    }) => Promise<
+      {
+        id: string;
+        url: string;
+        nick: string;
+        mailMd5: string;
+        link: string;
+        comment: string;
+        commentText: string;
+        created: number;
+        avatar: string;
+        relativeTime: string;
+      }[]
+    >;
+  };
 }
