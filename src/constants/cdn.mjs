afa5bf98f4697483f52d1c0509324f5fc0903bd4
HPCesia <me@hpcesia.com>
2025-04-05 19:07:51 +0800

    feat(comment): move comment assets to cdn
    - Remove Twikoo's local style.
    - Remove local package of Artalk and Waline to minify styles
    
    This commit is because of unused comment styles in build packaged css.
    
    https://github.com/vitejs/vite/pull/19418 might have helped, but Astro didn't use this experimental feature (https://github.com/withastro/astro/pull/13420).

--- src/constants/cdn.mjs
+++ src/constants/cdn.mjs
@@ -1,6 +1,8 @@
 export const CDN = {
-  twikoo: 'https://cdn.jsdelivr.net/npm/twikoo@1.6.41/dist/twikoo.nocss.js',
+  twikoo: 'https://cdn.jsdelivr.net/npm/twikoo@1.6.42/dist/twikoo.min.js',
+  giscus: 'https://giscus.app/client.js',
+  artalkCss: 'https://cdnjs.cloudflare.com/ajax/libs/artalk/2.9.1/Artalk.css',
+  walineCss: 'https://unpkg.com/@waline/client@v3/dist/waline.css',
   mathjaxFont:
     'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/output/chtml/fonts/woff-v2',
-  giscus: 'https://giscus.app/client.js',
 };
