dc34a4a4a1ffca644972b75a2d85ff0a1742090e
HPCesia <me@hpcesia.com>
2025-04-18 15:01:29 +0800

    fix: callout text

--- src/plugins/remark-obsidian-callout.ts
+++ src/plugins/remark-obsidian-callout.ts
@@ -147,6 +147,7 @@ export const remarkObsidianCallout: RemarkPlugin = function () {
                   type: 'text',
                   value: remainContents,
                 },
+                ...paragraph.children.slice(1),
               ],
             },
           ],
