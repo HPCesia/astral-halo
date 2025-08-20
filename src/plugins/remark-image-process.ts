eeb99c7d24d87cc6aea13fc9a86858b19012be29
HPCesia <me@hpcesia.com>
2025-08-20 15:24:40 +0800

    fix: local image zoom

--- src/plugins/remark-image-process.ts
+++ src/plugins/remark-image-process.ts
@@ -4,10 +4,8 @@ import { visit } from 'unist-util-visit';
 export const remarkImageProcess: RemarkPlugin = function () {
   return (tree) => {
     visit(tree, 'image', (node) => {
-      // 添加 data-zoom 属性到图片节点
       node.data = node.data || {};
       node.data.hProperties = node.data.hProperties || {};
-      node.data.hProperties['data-zoom'] = '';
       // lazyload
       node.data.hProperties.loading = 'lazy';
       // async decode
